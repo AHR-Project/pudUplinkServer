7740e201f2ece1d1c04e2ab78d1040f37feee2bd
Ferry Huberts <f.huberts@mindef.nl>
2011-10-31 11:55:07 +0100

    add a log for startup
    Signed-off-by: Ferry Huberts <f.huberts@mindef.nl>

--- src/main/java/nl/mindef/c2sc/nbs/olsr/pud/uplink/server/Main.java
+++ src/main/java/nl/mindef/c2sc/nbs/olsr/pud/uplink/server/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	private static Logger logger = Logger.getLogger(Main.class.getName());
 
 	public static void main(String[] args) {
+		logger.info("RelayServer starting");
 		try {
 			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 					"classpath:META-INF/spring/application-context.xml");
