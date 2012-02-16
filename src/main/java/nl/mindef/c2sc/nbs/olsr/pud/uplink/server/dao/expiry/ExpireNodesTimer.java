5502154953075001d20694352ba230f11d315394
Ferry Huberts <f.huberts@mindef.nl>
2012-02-16 09:10:01 +0100

    Split expiry of nodes and its timer
    to force a transaction (call a method on the proxy)
    
    Signed-off-by: Ferry Huberts <f.huberts@mindef.nl>

---
+++ src/main/java/nl/mindef/c2sc/nbs/olsr/pud/uplink/server/dao/expiry/ExpireNodesTimer.java
@@ -0,0 +1,91 @@
+package nl.mindef.c2sc.nbs.olsr.pud.uplink.server.dao.expiry;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+
+public class ExpireNodesTimer {
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+
+	private ExpireNodes expireNodes;
+
+	/**
+	 * @param expireNodes
+	 *          the expireNodes to set
+	 */
+	@Required
+	public final void setExpireNodes(ExpireNodes expireNodes) {
+		this.expireNodes = expireNodes;
+	}
+
+	/** the expiry interval (millseconds) */
+	private long interval;
+
+	/**
+	 * @param interval
+	 *          the interval to set
+	 */
+	@Required
+	public void setInterval(long interval) {
+		this.interval = interval;
+	}
+
+	/**
+	 * Timer task that does the actual expiry of out-of-date and empty objects
+	 */
+	private class ExpiryTimerTask extends TimerTask {
+		@SuppressWarnings("hiding")
+		private Logger logger;
+
+		@SuppressWarnings("hiding")
+		private ExpireNodes expireNodes;
+
+		/**
+		 * @param logger
+		 *          the logger
+		 * @param expireNodes
+		 *          the expireNodes
+		 */
+		public ExpiryTimerTask(Logger logger, ExpireNodes expireNodes) {
+			super();
+			this.logger = logger;
+			this.expireNodes = expireNodes;
+		}
+
+		@Override
+		public void run() {
+			try {
+				this.expireNodes.expire();
+			} catch (Throwable e) {
+				this.logger.error("error during expiry", e);
+			}
+		}
+	}
+
+	/*
+	 * Main
+	 */
+
+	// FIXME timer must only start when init cycle is complete
+
+	/** the timer from which the expiry task runs */
+	private Timer timer = null;
+
+	public void init() {
+		if (this.interval <= 0) {
+			return;
+		}
+
+		this.timer = new Timer(this.getClass().getSimpleName());
+		this.timer.scheduleAtFixedRate(new ExpiryTimerTask(this.logger, this.expireNodes), this.interval, this.interval);
+	}
+
+	public void uninit() {
+		if (this.timer != null) {
+			this.timer.cancel();
+			this.timer = null;
+		}
+	}
+}
---
+++ src/main/java/nl/mindef/c2sc/nbs/olsr/pud/uplink/server/dao/expiry/impl/ExpireNodesImpl.java
@@ -0,0 +1,114 @@
+package nl.mindef.c2sc.nbs.olsr.pud.uplink.server.dao.expiry.impl;
+
+import nl.mindef.c2sc.nbs.olsr.pud.uplink.server.dao.ClusterLeaderMsgs;
+import nl.mindef.c2sc.nbs.olsr.pud.uplink.server.dao.Nodes;
+import nl.mindef.c2sc.nbs.olsr.pud.uplink.server.dao.PositionUpdateMsgs;
+import nl.mindef.c2sc.nbs.olsr.pud.uplink.server.dao.Senders;
+import nl.mindef.c2sc.nbs.olsr.pud.uplink.server.dao.expiry.ExpireNodes;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Expire out-of-date PositionUpdate and ClusterLeader messages, and then remove empty Nodes and Senders. Do NOT remove
+ * empty RelayServers since these are statically configured
+ */
+@Repository
+public class ExpireNodesImpl implements ExpireNodes {
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+
+	/** the multiplier for the validity time */
+	private double validityTimeMultiplier = 3.0;
+
+	/**
+	 * @param validityTimeMultiplier
+	 *          the validityTimeMultiplier to set
+	 */
+	public void setValidityTimeMultiplier(double validityTimeMultiplier) {
+		this.validityTimeMultiplier = validityTimeMultiplier;
+	}
+
+	/** the Node DAO */
+	private Nodes nodes;
+
+	/**
+	 * @param nodes
+	 *          the nodes to set
+	 */
+	@Required
+	public void setNodes(Nodes nodes) {
+		this.nodes = nodes;
+	}
+
+	/** the PositionUpdateMsg DAO */
+	private PositionUpdateMsgs positionUpdateMsgs;
+
+	/**
+	 * @param positionUpdateMsgs
+	 *          the positionUpdateMsgs to set
+	 */
+	@Required
+	public void setPositions(PositionUpdateMsgs positionUpdateMsgs) {
+		this.positionUpdateMsgs = positionUpdateMsgs;
+	}
+
+	/** the ClusterLeaderMsg DAO */
+	private ClusterLeaderMsgs clusterLeaderMsgs;
+
+	/**
+	 * @param clusterLeaderMsgs
+	 *          the clusterLeaderMsgs to set
+	 */
+	@Required
+	public void setClusterLeaderMsgs(ClusterLeaderMsgs clusterLeaderMsgs) {
+		this.clusterLeaderMsgs = clusterLeaderMsgs;
+	}
+
+	/** the Sender DAO */
+	private Senders senders;
+
+	/**
+	 * @param senders
+	 *          the senders to set
+	 */
+	@Required
+	public void setSenders(Senders senders) {
+		this.senders = senders;
+	}
+
+	@Override
+	@Transactional
+	public void expire() {
+		if (this.logger.isDebugEnabled()) {
+			this.logger.debug("************************** expiry");
+		}
+
+		long utcTimestamp = System.currentTimeMillis();
+
+		try {
+			this.clusterLeaderMsgs.removeExpiredClusterLeaderMsg(utcTimestamp, this.validityTimeMultiplier);
+		} catch (Throwable e) {
+			this.logger.error("Removal of expired cluster leader messages failed", e);
+		}
+
+		try {
+			this.positionUpdateMsgs.removeExpiredPositionUpdateMsg(utcTimestamp, this.validityTimeMultiplier);
+		} catch (Throwable e) {
+			this.logger.error("Removal of expired position update messages failed", e);
+		}
+
+		try {
+			this.nodes.removeExpiredNodes();
+		} catch (Throwable e) {
+			this.logger.error("Removal of empty nodes failed", e);
+		}
+
+		try {
+			this.senders.removeExpiredSenders();
+		} catch (Throwable e) {
+			this.logger.error("Removal of empty senders failed", e);
+		}
+	}
+}
